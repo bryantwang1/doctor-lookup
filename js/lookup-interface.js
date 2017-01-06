20566b97243aba24e72da676e7017b08097a559b
Bryant <bryant.m.wang@gmail.com>
2017-01-06 11:43:03 -0800

    Add ability for infinite scrolling, primarily in the issueDisplay function in lookup-interface.js. Adjust searchByIssue prototype method in lookup-object to produce new results when infinite scrolling occurs. Adjust some minor styling.

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,26 +1,27 @@
 var Lookup = require("./../js/lookup-object.js").lookupObject;
 
-function issueDisplay(results) {
-  $("#results-display").empty();
+function issueDisplay(results, doctorCount) {
+  var savedCount = doctorCount;
 
-  var doctorCount = 0;
   results.data.forEach(function(doctor) {
     doctorCount++;
 
     $("#results-display").append(
-      "<div class=\"item-display\">" +
-        "<img src=\"" + doctor.profile.image_url + "\">" +
-        "<div class=\"detail-container\">" +
-          "<h5>Name: </h5><p>" + doctor.profile.first_name + " " + doctor.profile.last_name + "</p>" +
-        "</div>" +
-        "<div class=\"detail-container\">" +
-          "<h5>Gender: </h5><p>" + ((doctor.profile.gender == "male") ? "<span class=\"gender-marker\">&#9794;</span>" : "<span class=\"gender-marker\">&#9792;</span>") + " " + doctor.profile.gender + "</p>" +
-        "</div>" +
-        "<div class=\"detail-container\" \"specialty-container\">" +
-          "<h5>Specialties: </h5><ul id=\"doctor-" + doctorCount + "\"></ul>" +
-        "</div>" +
-        "<div class=\"detail-container\">" +
-          "<h5>Bio: </h5><p>" + doctor.profile.bio + "</p>" +
+      "<div class=\"initial-hider\" id=\"hider-" + doctorCount + "\">" +
+        "<div class=\"item-display\">" +
+          "<img src=\"" + doctor.profile.image_url + "\">" +
+          "<div class=\"detail-container\">" +
+            "<h5>Name: </h5><p>" + doctor.profile.first_name + " " + doctor.profile.last_name + "</p>" +
+          "</div>" +
+          "<div class=\"detail-container\">" +
+            "<h5>Gender: </h5><p>" + ((doctor.profile.gender == "male") ? "<span class=\"gender-marker\">&#9794;</span>" : "<span class=\"gender-marker\">&#9792;</span>") + " " + doctor.profile.gender + "</p>" +
+          "</div>" +
+          "<div class=\"detail-container\" \"specialty-container\">" +
+            "<h5>Specialties: </h5><ul id=\"doctor-" + doctorCount + "\"></ul>" +
+          "</div>" +
+          "<div class=\"detail-container\">" +
+            "<h5>Bio: </h5><p>" + doctor.profile.bio + "</p>" +
+          "</div>" +
         "</div>" +
       "</div>"
     );
@@ -28,17 +29,46 @@ function issueDisplay(results) {
     doctor.specialties.forEach(function(specialty) {
       $("#results-display").find("#doctor-" + doctorCount).append("<li>" + specialty.name + "</li>");
     });
+
+    //  $("#hider-" + doctorCount).fadeIn(1000).queue();
   });
+
+  function fadeDoctorsIn() {
+    console.log("fading in");
+    setTimeout(function() {
+      savedCount++;
+      if(savedCount <= doctorCount) {
+        $("#hider-" + savedCount).fadeIn(600);
+        fadeDoctorsIn();
+      }
+    }, 220);
+  }
+
+  fadeDoctorsIn();
 }
 
 $(function() {
-  $("#issue-search").submit(function(event) {
-    event.preventDefault();
+  console.log("ready");
+  var newLookup = new Lookup();
+  var userIssue = "";
 
-    var userIssue = $("#medical-issue").val();
+  $("#issue-search").submit(function(event) {
+    console.log("first count: " + newLookup.doctorCount);
+    event.preventDefault();
+    $("#results-display").empty();
+    newLookup.doctorCount = 0;
+    console.log("second count: " + newLookup.doctorCount);
+
+    userIssue = $("#medical-issue").val();
     $("#medical-issue").val("");
 
-    var newLookup = new Lookup();
     newLookup.searchByIssue(userIssue, issueDisplay);
   });
+
+  $(window).scroll(function() {
+    if($(document).height() - $(window).height() == $(window).scrollTop()) {
+      console.log("third count: " + newLookup.doctorCount);
+      newLookup.searchByIssue(userIssue, issueDisplay);
+    }
+  })
 });
--- js/lookup-object.js
+++ js/lookup-object.js
@@ -1,16 +1,22 @@
 var apiKey = require("./../.env").apiKey;
 
-function Lookup() {}
+function Lookup() {
+  this.doctorCount = 0;
+}
 
 Lookup.prototype.searchByIssue = function(medicalIssue, displayFunction) {
-  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" +  medicalIssue + "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=5&user_key=" + apiKey)
+  var doctorCount = this.doctorCount;
+
+  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" +  medicalIssue + "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=" + doctorCount + "&limit=10&user_key=" + apiKey)
   .then(function(response) {
     console.log(response);
-    displayFunction(response);
+    displayFunction(response, doctorCount);
   })
   .fail(function(error) {
     console.log("fail");
   });
+
+  this.doctorCount += 10;
 };
 
 exports.lookupObject = Lookup;
