09c2ebeab65c05341daa39ec6782c2f6b9138de6
Bryant <bryant.m.wang@gmail.com>
2017-01-06 10:35:37 -0800

    Add display function to lookup-interface.js to further separate front and back ends. Add some sass styling.

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,5 +1,36 @@
 var Lookup = require("./../js/lookup-object.js").lookupObject;
 
+function issueDisplay(results) {
+  $("#results-display").empty();
+
+  var doctorCount = 0;
+  results.data.forEach(function(doctor) {
+    doctorCount++;
+
+    $("#results-display").append(
+      "<div class=\"item-display\">" +
+        "<img src=\"" + doctor.profile.image_url + "\">" +
+        "<div class=\"detail-container\">" +
+          "<h5>Name: </h5><p>" + doctor.profile.first_name + " " + doctor.profile.last_name + "</p>" +
+        "</div>" +
+        "<div class=\"detail-container\">" +
+          "<h5>Gender: </h5><p>" + ((doctor.profile.gender == "male") ? "<span class=\"gender-marker\">&#9794;</span>" : "<span class=\"gender-marker\">&#9792;</span>") + " " + doctor.profile.gender + "</p>" +
+        "</div>" +
+        "<div class=\"detail-container\" \"specialty-container\">" +
+          "<h5>Specialties: </h5><ul id=\"doctor-" + doctorCount + "\"></ul>" +
+        "</div>" +
+        "<div class=\"detail-container\">" +
+          "<h5>Bio: </h5><p>" + doctor.profile.bio + "</p>" +
+        "</div>" +
+      "</div>"
+    );
+
+    doctor.specialties.forEach(function(specialty) {
+      $("#results-display").find("#doctor-" + doctorCount).append("<li>" + specialty.name + "</li>");
+    });
+  });
+}
+
 $(function() {
   $("#issue-search").submit(function(event) {
     event.preventDefault();
